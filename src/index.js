2bfeb038e0304611219ef9e6ccc0a2df38928c98
TanyaEdyna <t.yamkova@gmail.com>
2023-03-29 12:54:00 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -15,55 +15,60 @@ refs.inputSearchBox.addEventListener('input', debounce(handleSearchCountry, DEBO
 
 function handleSearchCountry(event) {
     event.preventDefault();
-    const seekedCountry = refs.inputSearchBox.value.trim();//отримую значення яке ввів користувач в поле інпут
-    if (seekedCountry === null) {
-        refs.countryInfo.innerHTML = '';
+    const seekedCountry = event.target.value.trim();//отримую значення яке ввів користувач в поле інпут
+    if (seekedCountry === '') {
         refs.countryList.innerHTML = '';
-        return
+        refs.countryInfo.innerHTML = '';
+        
+        return;
     }
     console.log(seekedCountry);
-
-  fetchCountries(seekedCountry);
-}
 //якщо повертається більше 10 країн, то повідомлення "Too many matches found. Please enter a more specific name."
 //якщо від 2-10, то видає  список країн ( складається з прапора та назви країни)
 //якщо одна країна - масив (прапор, назва, столиця, населення і мови.)
-function createListOfSearchedCountries(data) {
-    if (data.length > 10) {
-        Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
-    } else if (data.length >= 2 && data.lenght <= 10) {
-        //тут передати функцію яка видає  список країн
-        createCountryListMarkup(data);
-    } else if (data.length === 1) {
-        createCountryInfoMarkup(data[0]);//передає перший елемент з масиву
-    }
 
-
+    fetchCountries(seekedCountry).then(data => {
+    
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+         if (data.length > 10) {
+            Notiflix.Report.info('Too many matches found. Please enter a more specific name.');
+        } else if (data.length >= 2 && data.length <= 10) {
+        refs.countryList.insertAdjacentHTML('afterbegin', createCountryListMarkup(data));
+        } else if (data.length === 1) {
+        refs.countryInfo.insertAdjacentHTML('afterbegin', createCountryInfoMarkup(data[0]));
+         } else {
+             Notiflix.Report.warning('Oops, there is no country with that name');
+        }
+        
+  })
 }
-const { name, capital, population, flags, languages } = data;
-console.log(data);//????
 
-function createCountryListMarkup({ flags, name }) {
-    const countryMarkup = `<li class="country_list_item">
+function createCountryListMarkup(data) {
+    const countryMarkup = data.map(({ name, flags }) => {
+        console.log(data);
+        return `<li class="country_list_item">
     <img class="country_img" src="${flags.svg}" alt="${name}" width="100">
     <p class="country_name">${name.official}</p>
-    </li>`;
-    refs.countryList.insertAdjacentHTML('afterbegin', countryMarkup);
-
+    </li>`
+    }).join('');
+    return countryMarkup;
+   
 }
 
-function createCountryInfoMarkup({ flags, name, capital, population, languages }) {
-    countryMarkup = ` <img class="country_img" src="${flags.svg}" alt="${name}" width="100">
-<p class="country_name">${name.official}</p>
-<ul class="country_info_list">
-  <li class="country_info_item">Capital <span class="country_info_capital">${capital}</span></li>
-
-  <li class="country_info_item">Population <span class="country_info_population">${population}</span></li>
-
-  <li class="country_info_item">Languages <span class="country_info_languages">${languages}</span></li>
-</ul>`
-    refs.countryInfo.insertAdjacentHTML('afterbegin', countryMarkup);
-
+function createCountryInfoMarkup(data) {
+    const countryMarkup = data.map(({ name, flags, capital, population, languages }) => {
+        return ` 
+        <img class="country_img" src="${flags.svg}" alt="${name}" width="100">
+        <p class="country_name">${name.official}</p>
+        <ul class="country_info_list">
+            <li class="country_info_item">Capital <span class="country_info_capital">${capital}</span></li>
+            <li class="country_info_item">Population <span class="country_info_population">${population}</span></li>
+            <li class="country_info_item">Languages <span class="country_info_languages">${languages.join(', ')}</span></li>
+        </ul>`
+    }).join('');
+    return countryMarkup
+    
 }
 
 
